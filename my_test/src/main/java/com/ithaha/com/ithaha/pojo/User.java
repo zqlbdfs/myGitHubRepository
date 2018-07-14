fd5546eb320c35dbfd18d5a946006cbdf3cbe009
zqlbdfs <850904743@qq.com>
2018-07-14 16:42:30 +0800

    add Service and pojo

---
+++ my_test/src/main/java/com/ithaha/com/ithaha/pojo/User.java
@@ -0,0 +1,16 @@
+package com.ithaha.com.ithaha.pojo;
+
+/**
+ * @author lf
+ * @Title: User
+ * @Description: TODO
+ * @date 2018/7/14 16:41
+ */
+public class User {
+
+    private String name;
+    private int id;
+
+    public User(){}
+
+}
---
+++ my_test/src/main/java/com/ithaha/service/TestService.java
@@ -0,0 +1,15 @@
+package com.ithaha.service;
+
+/**
+ * @author lf
+ * @Title: TestService
+ * @Description: TODO
+ * @date 2018/7/14 16:40
+ */
+public class TestService {
+
+    public void say(){
+
+
+    }
+}
