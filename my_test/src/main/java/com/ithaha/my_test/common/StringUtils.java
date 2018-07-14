d6b5e476b5940be40298c2ae4d2dadd7fad1ae6f
zqlbdfs <850904743@qq.com>
2018-07-14 16:51:19 +0800

    add common

---
+++ my_test/src/main/java/com/ithaha/my_test/common/StringUtils.java
@@ -0,0 +1,14 @@
+package com.ithaha.my_test.common;
+
+/**
+ * @author lf
+ * @Title: StringUtils
+ * @Description: TODO
+ * @date 2018/7/14 16:50
+ */
+public class StringUtils {
+
+    public String getString(){
+        return "abc";
+    }
+}
